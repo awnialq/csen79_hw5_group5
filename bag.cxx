c7bab010317f85c362d0c3f6a465f053f539dd34
awni alquraini <aalquraini@linux10609.dc.engr.scu.edu>
2025-10-19 11:38:02 -0700

    mostly finished the implementation. just need to account for overflow

--- bag.cxx
+++ bag.cxx
@@ -52,6 +52,11 @@ namespace csen79 {
     };
 
     void Bag::resize() {
+        if(size == 0){
+            size = 1;
+            data = new Data[size];
+            return;
+        }
         Data *temp = new Data[size * 2];
         memcpy(temp, data, sizeof(Data) * size);
         delete [] data;
@@ -59,6 +64,22 @@ namespace csen79 {
         size = size * 2;
     }
 
-    Bag::Data Bag::pop() {return 0;};
-    void Bag::print() const {};
+    Bag::Data Bag::pop() {
+        if(last <= 0){
+            throw std::out_of_range("pop from empty bag");
+        }
+        return data[--last];
+    };
+
+    void Bag::print() const {
+        if(last == 0){
+            std::cout << "Nothing in bag to print" << std::endl;
+            return;
+        }
+        std::cout << "Bag contents: ";
+        for(int i = 0; i < last; i++){
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    };
 }
